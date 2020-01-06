83917b97d29d4ee46ba3d92316c6e44cbfb15ad2
al7262 <alta5@alta5>
2020-01-06 17:48:18 +0700

    add file

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../style/bootstrap.min.css';
 import '../style/style.css';
-import logo from '../logo.svg'
+import logo from '../image/logo.svg'
 
 class Header extends React.Component{
     render(){
